5a512794eeeb9ef480105a2613192ba76c37e90a
Akyuna Akish <akyunaakish@gmail.com>
2017-11-27 15:21:56 -0700

    finished sprial matrix and vowls sections

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,68 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// solution #1
+const matrix = (n) => {
+    // --------------------CREATE-RESULTS-INITIALLY--------------------
+
+    let results = [];
+
+    for (let i = 1; i <= n; i++) {
+        results.push([]);
+    }
+
+    // -----------------CREATE-RESULTS-INITIALLY-END-----------------
+
+    // --------------------------------------------------------------
+
+    // ---------------------INSERT-MATRIX-VALUES---------------------
+
+    let count = 1;
+
+    let startColumn = 0;
+    let endColumn = n - 1;
+
+    let startRow = 0;
+    let endRow = n - 1;
+
+    while (count <= n * n) {
+
+        // left to right for loop
+        for (let i = startColumn; i <= endColumn; i++) {
+            results[startRow][i] = count;
+            count++;
+        }
+
+        startRow++;
+
+        // top to bottom for loop
+        for (let i = startRow; i <= endRow; i++) {
+            results[i][endColumn] = count;
+            count++;
+        }
+
+        endColumn--;
+
+        // right to left for loop
+        for (let i = endColumn; i >= startColumn; i--) {
+            results[endRow][i] = count;
+            count++;
+        }
+
+        endRow--;
+
+        // bottom to top for loop
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startColumn] = count;
+            count++;
+        }
+
+        startColumn++;
+    }
+
+    // -------------------INSERT-MATRIX-VALUES-END-------------------
+
+    return results;
+}
 
 module.exports = matrix;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// solution #3
+const vowels = (str) => {
+    // g makes sure the regex doesn't 
+    // stop at the first match and keeps looking for other matches
+    // match will return an array of matches, if no matches are found
+    // it will return null
+    const matches = str.match(/[aeiou]/gi);
+    
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// solution #1
+// const vowels = (str) => {
+//     return str.split('').reduce((count, char) => {
+//         const regex = new RegExp('[aeiou]', 'i').test(char);
+
+//         if (regex) count++;
+
+//         return count;
+//     }, 0);
+// }
+
+// solution #2
+// const vowels = (str) => {
+//     let count = 0;
+//     const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) count++;
+//     }
+
+//     return count;
+// }
